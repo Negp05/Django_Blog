d34429189e207b64cb391d90dabbb20309f7743e
Negp05 <norvisg34@gmail.com>
2025-10-12 16:03:48 -0500

    Guardando cambios locales antes del merge con master

--- Procfile
+++ Procfile
@@ -1,2 +1,4 @@
-release: python manage.py este_comando_no_existe 
-web: python manage.py collectstatic --noinput && gunicorn blog.wsgi:application --bind 0.0.0.0:$PORT 
+del Procfilerelease:
+python
+manage.py
+este_comando_no_existe
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views  # ← IMPORTANTE: Agrega esto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +11,8 @@ urlpatterns = [
     
     path("", include(("auth_perfiles.urls", "auth_perfiles"), namespace="auth_perfiles")),
     path("", TemplateView.as_view(template_name="base.html"), name="home"),
+    
+    # === AGREGA ESTAS RUTAS DE AUTENTICACIÓN ===
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
